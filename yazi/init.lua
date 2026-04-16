745078d66b520978eae53175e5363097a77c9ac3
Ivo Bathke <ivo.bathke@gmail.com>
2026-04-16 14:21:34 +0200

    feat: add bookmarks plugin with keybindings for save, jump, and delete operations

--- yazi/init.lua
+++ yazi/init.lua
@@ -4,3 +4,21 @@ require("duckdb"):setup({
   minmax_column_width = 21,
   column_fit_factor = 10.0
 })
+
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false, mode="dir" },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	show_keys = false,
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
