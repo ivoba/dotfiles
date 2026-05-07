2b1e5604036554a15ffe990dab8ed6e3e26043be
Ivo Bathke <ivo.bathke@gmail.com>
2026-05-07 16:00:16 +0200

    ♻️ refactor: replace bookmarks plugin with bunny for improved navigation

--- yazi/init.lua
+++ yazi/init.lua
@@ -5,20 +5,20 @@ require("duckdb"):setup({
   column_fit_factor = 10.0
 })
 
-require("bookmarks"):setup({
-	last_directory = { enable = false, persist = false, mode="dir" },
-	persist = "all",
-	desc_format = "full",
-	file_pick_mode = "hover",
-	custom_desc_input = false,
-	show_keys = false,
-	notify = {
-		enable = true,
-		timeout = 1,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
-	},
+require("bunny"):setup({
+  hops = {
+    { key = "/",          path = "/",              desc = "Root"         },
+    { key = "~",          path = "~",              desc = "Home"         },
+    { key = "d",          path = "~/Downloads",    desc = "Downloads"    },
+    { key = "k",          path = "~/kDrive",       desc = "kDrive"    },
+    { key = "s",          path = "~/Desktop",      desc = "Desktop"      },
+    { key = "D",          path = "~/Documents",    desc = "Documents"    },
+    { key = "c",          path = "~/.config",      desc = "Config files" },
+    -- key and path attributes are required, desc is optional
+  },
+  desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+  ephemeral = true, -- Enable ephemeral hops, default is true
+  tabs = true, -- Enable tab hops, default is true
+  notify = true, -- Notify after hopping, default is false
+  fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
 })
